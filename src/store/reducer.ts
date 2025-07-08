52477df6f54775f692a1d9c94b4da202256b202d
Brian Harvey <brianpaulharvey@gmail.com>
2025-07-08 15:03:18 -0700

    adds placeholder structure

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 
 // project imports
 import snackbarReducer from './slices/snackbar';
+import userReducer from './slices/user';
 
 // ==============================|| COMBINE REDUCER ||============================== //
 
 const reducer = combineReducers({
-  snackbar: snackbarReducer
+  snackbar: snackbarReducer,
+  user: userReducer
 });
 
 export default reducer;
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,18 +1,111 @@
+import { useState } from 'react';
+
 // material-ui
-import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
+import EarningCard from './EarningCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import UserList from './UserList';
+import { gridSpacing } from 'store/constant';
+
+// assets
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
 
 export default function DashboardPage() {
+  const [anchorEl, setAnchorEl] = useState<Element | (() => Element) | null | undefined>(null);
+  const handleClick = (event: React.MouseEvent) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // Ugly ugly ugly quick fix
+  const isLoading = false;
+
   return (
-    <MainCard title="Dashboard">
-      <Typography variant="body2">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-        reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
-        officiate descent molls anim id est labours.
-      </Typography>
-    </MainCard>
+    <Grid container spacing={gridSpacing}>
+      <Grid size={12}>
+        <MainCard title="QuickBooks Pro">
+          <Grid container spacing={gridSpacing}>
+            <Grid size={{ lg: 3, md: 3, sm: 6, xs: 12 }}>
+              <EarningCard isLoading={isLoading} />
+            </Grid>
+            <Grid size={{ lg: 3, md: 3, sm: 6, xs: 12 }}>
+              <EarningCard isLoading={isLoading} />
+            </Grid>
+            <Grid size={{ lg: 3, md: 3, sm: 6, xs: 12 }}>
+              <EarningCard isLoading={isLoading} />
+            </Grid>
+            <Grid size={{ lg: 3, md: 3, sm: 6, xs: 12 }}>
+              <EarningCard isLoading={isLoading} />
+            </Grid>
+          </Grid>
+        </MainCard>
+      </Grid>
+      <Grid size={12}>
+        <MainCard title="Analytics">
+          <Grid container spacing={gridSpacing}>
+            <Grid size={12}>
+              <TotalGrowthBarChart isLoading={isLoading} />
+            </Grid>
+          </Grid>
+        </MainCard>
+      </Grid>
+      <Grid size={12}>
+        <MainCard title="Inventory">
+          <Grid container spacing={gridSpacing}>
+            <Grid size={12}>
+              <TotalGrowthBarChart isLoading={isLoading} />
+            </Grid>
+          </Grid>
+        </MainCard>
+      </Grid>
+      <Grid size={12}>
+        <MainCard title="Employees">
+          <Grid size={12}>
+            <UserList />
+            <Grid container spacing={gridSpacing} sx={{ justifyContent: 'space-between' }}>
+              <Grid>
+                <Pagination count={10} color="primary" />
+              </Grid>
+              <Grid>
+                <Button size="large" sx={{ color: 'grey.900' }} color="secondary" endIcon={<ExpandMoreRoundedIcon />} onClick={handleClick}>
+                  10 Rows
+                </Button>
+                {anchorEl && (
+                  <Menu
+                    id="menu-employee-list-style1"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    variant="selectedMenu"
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right'
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}> 10 Rows</MenuItem>
+                    <MenuItem onClick={handleClose}> 20 Rows</MenuItem>
+                    <MenuItem onClick={handleClose}> 30 Rows </MenuItem>
+                  </Menu>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+        </MainCard>
+      </Grid>
+    </Grid>
   );
 }
