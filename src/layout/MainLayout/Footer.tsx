88ad9f202d0b4f627482a609c10d1f30b4076d41
Sam McLain-Jespersen <heyheysamj@gmail.com>
2025-07-09 16:14:08 -0700

    update footer text

--- src/layout/MainLayout/Footer.tsx
+++ src/layout/MainLayout/Footer.tsx
@@ -1,7 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom';
 
 // material-ui
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
@@ -9,12 +9,12 @@ export default function Footer() {
   return (
     <Stack direction="row" sx={{ alignItems: 'center', justifyContent: 'space-between', pt: 3, mt: 'auto' }}>
       <Typography variant="caption">
-        &copy; All rights reserved{' '}
-        <Typography component={Link} href="https://codedthemes.com" underline="hover" target="_blank" color="secondary.main">
+        &copy; All rights reserved Allyvia
+        {/* <Typography component={Link} href="https://codedthemes.com" underline="hover" target="_blank" color="secondary.main">
           CodedThemes
-        </Typography>
+        </Typography> */}
       </Typography>
-      <Stack direction="row" sx={{ gap: 1.5, alignItems: 'center', justifyContent: 'space-between' }}>
+      {/* <Stack direction="row" sx={{ gap: 1.5, alignItems: 'center', justifyContent: 'space-between' }}>
         <Link
           component={RouterLink}
           to="https://x.com/codedthemes"
@@ -35,7 +35,7 @@ export default function Footer() {
         >
           Discord
         </Link>
-      </Stack>
+      </Stack> */}
     </Stack>
   );
 }
