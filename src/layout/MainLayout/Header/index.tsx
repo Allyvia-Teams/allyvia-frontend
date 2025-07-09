8ae727baf7f0c88230ccfc0107c5d9b1e08a9323
Sam McLain-Jespersen <heyheysamj@gmail.com>
2025-07-09 16:14:37 -0700

    center the logo

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -31,7 +31,7 @@ export default function Header() {
     <>
       {/* logo & toggler button */}
       <Box sx={{ width: downMD ? 'auto' : 228, display: 'flex' }}>
-        <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
+        <Box component="span" sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1, textAlign: 'center' }}>
           <LogoSection />
         </Box>
         {!isHorizontal && (
