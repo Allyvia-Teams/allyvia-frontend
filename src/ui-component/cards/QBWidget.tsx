eb63acf6e673677bbc860eb50acb0aee5b01b094
Sam McLain-Jespersen <heyheysamj@gmail.com>
2025-07-11 14:28:13 -0700

    QBWidget skeleton error refresh state

--- src/ui-component/cards/QBWidget.tsx
+++ src/ui-component/cards/QBWidget.tsx
@@ -1,19 +1,28 @@
 // material-ui
+import { Box, Typography } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
 import { mediumWidgetHeight } from 'store/constant';
+import { useState } from 'react';
 
 // ==============================|| SKELETON - QB-WIDGET ||============================== //
 
-export default function QBWidgetSkeleton() {
-
+export default function QBWidgetSkeleton({refetch, isLoading, isError}: {refetch?: () => void, isLoading?: boolean, isError?: boolean}) {
+  const [isSpinning, setIsSpinning] = useState(false);
+  const shimmer = 'linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.8), transparent)'
+  const shimmerAnimation = 'shimmer 750ms ease-in-out infinite'
   return (
     <Card 
+        onClick={refetch} 
+        onMouseEnter={() => {
+          setIsSpinning(true);
+          setTimeout(() => setIsSpinning(false), 800);
+        }}
       sx={{ 
         height: `${mediumWidgetHeight}px`, 
-        bgcolor: "primary.light",
+        bgcolor: isLoading ? "primary.light" : isError ? "error.light" : "primary.light",
+        border: isError && !isLoading ? "1px solid" : "none",
+        borderColor: isError && !isLoading ? "error.main" : "none",
         position: 'relative',
         overflow: 'hidden',
         '&::before': {
@@ -25,9 +34,10 @@ export default function QBWidgetSkeleton() {
           bottom: 0,
           width: '100%',
           height: '100%',
-          background: 'linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.8), transparent)',
-          animation: 'shimmer 750ms ease-in-out infinite',
+          background: isLoading ? shimmer : isError ? "error.light" : shimmer,
+          animation: isLoading ? shimmerAnimation : isError ? 'none' : shimmerAnimation,
           zIndex: 1,
+          cursor: 'pointer',
         },
         '@keyframes shimmer': {
           '0%': {
@@ -38,6 +48,37 @@ export default function QBWidgetSkeleton() {
           },
         },
       }} 
-    />
+    >
+      {!isLoading && isError && (
+        <Box 
+           sx={{ display: 'flex', width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', zIndex: 2 }}>          
+          <Typography sx={{ overflow:'hidden', position: 'absolute', top: 8, zIndex: 2, color: 'error.dark', fontSize: '1rem', fontWeight: 500 }}>
+            Error fetching data
+          </Typography>
+          <Box flexGrow={1} />
+          <RefreshIcon 
+             sx={{ 
+               fontSize: '80px', 
+               cursor: 'pointer', 
+               zIndex: 2, 
+               color: 'error.dark',
+               animation: isSpinning ? 'spin 0.8s ease forwards' : 'none',
+               '@keyframes spin': {
+                 '0%': {
+                   transform: 'rotate(0deg)'
+                 },
+                 '100%': {
+                   transform: 'rotate(360deg)'
+                 }
+               }
+             }} 
+           />
+          <Box flexGrow={1} />
+          <Typography sx={{ overflow:'hidden', position: 'absolute', bottom: 8, zIndex: 2, color: 'error.dark', fontSize: '1rem', fontWeight: 500 }}>
+            Click to refresh
+          </Typography>
+        </Box>
+      )}
+      </Card>
   );
 }
